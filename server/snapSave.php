eb5050b22e34fd31ccbd76e986c8bee2e188f7c9
Joran Quinten <joranquinten@hotmail.com>
2016-03-30 15:14:58 +0200

    added crude saving method

---
+++ server/snapSave.php
@@ -0,0 +1,70 @@
+<?php
+
+// Headers
+    session_start();
+    date_default_timezone_set('Europe/Amsterdam');
+    header('Content-Type: application/json');
+
+// Start timer
+    $time = microtime();
+    $time = explode(' ', $time);
+    $time = $time[1] + $time[0];
+    $start = $time;
+
+// Actual work, inserting a record
+
+    // Read data
+    $request_body = file_get_contents('php://input');
+    $data = json_decode($request_body, true);
+
+    $camera_name = $data['cameraName'];
+    $lens_name = $data['lensName'];
+    $file_name = $data['fileName'];
+    $series_name = $data['seriesName'];
+    $focal_length = $data['focalLength'];
+    $focal_distance = $data['focalDistance'];
+    $aperture_size = $data['apertureSize'];
+    $file_date = $data['fileDate'];
+    $location = $data['location'];
+
+    try {
+        $dbh = new PDO('mysql:host=localhost;dbname=db_snaps', 'root', '');
+
+        $sql = ' INSERT INTO snapshots ';
+        $sql .= ' (camera_name, lens_name, file_name, series_name, focal_length, focal_distance, aperture_size, file_date, location) ';
+        $sql .= ' VALUES ';
+        $sql .= ' ("'.$camera_name.'", "'.$lens_name.'", "'.$file_name.'", "'.$series_name.'", "'.$focal_length.'", "'.$focal_distance.'", "'. $aperture_size.'", "'.$file_date.'", "'. $location.'"); ';
+
+        $dbh->query($sql);
+        $dbh = null;
+
+        $success = true;
+
+    } catch (PDOException $e) {
+        print "Error!: " . $e->getMessage() . "<br/>";
+        die();
+    }
+
+// Stop timer
+    $time = microtime();
+    $time = explode(' ', $time);
+    $time = $time[1] + $time[0];
+    $finish = $time;
+    $total_time = round(($finish - $start), 4);
+
+// Generate readebly result
+$response = array(
+    'meta' => array(
+        'code' => 200,
+        'response_time' => array(
+            'time' => $total_time,
+            'measure' => 'seconds'
+            )
+        ),
+    'result' => array(
+        'success' => true
+    )
+);
+
+echo json_encode($response);
+?>
--- source/modules/log/log.js
+++ source/modules/log/log.js
@@ -7,19 +7,10 @@
     .controller('log', log);
 
   /* @ngInject */
-  function log(toastr, logService) {
+  function log(toastr, logService, $http) {
 
     var vm = this;
     // vars
-    vm.selectedCamera = 'ILCE-6000';
-    vm.selectedLens = 'Samyang 12mm f/2.0 NCS CS Sony';
-    vm.selectedFilePattern = 'DSC_#####';
-    vm.selectedSeriesName = '';
-    vm.selectedFileNumber = '';
-    vm.selectedLocation = '';
-    vm.selectedFocalLength = '';
-    vm.selectedFocalDisctance = '';
-    vm.selectedAperture = '';
 
     // functions
     vm.availableCameras = availableCameras();
@@ -27,30 +18,44 @@
     vm.availableApertures = availableApertures();
 
     vm.snap = snap;
+    vm.reset = reset;
 
-    vm.success = success;
-    vm.warning = warning;
-    vm.info = info;
-    vm.error = error;
+    setInitValues();
 
     ////////////////
 
+    function setInitValues(){
+
+        vm.selectedCamera = 'ILCE-6000';
+        vm.selectedLens = 'Samyang 12mm f/2.0 NCS CS Sony';
+        vm.selectedFilePattern = 'DSC_#####';
+        vm.selectedSeriesName = '';
+        vm.selectedFileNumber = '';
+        vm.selectedLocation = '';
+        vm.selectedFocalLength = '';
+        vm.selectedFocalDistance = '';
+        vm.selectedAperture = '';
+
+        vm.seriesOpen = true;
+        vm.NumberOfSaved = 0;
+
+    }
+
     function snap(){
-        toastr.info('Saving a snap from '+ vm.selectedCamera);
+        toastr.info('Saving a snap from '+ vm.selectedCamera +' to series '+ vm.selectedSeriesName +'.');
 
         console.log(vm);
-// Getting
-/*
-vm.selectedCamera
-vm.selectedLens
-vm.selectedFilePattern
-vm.selectedSeriesName
-vm.selectedFileNumber
-vm.selectedLocation
-vm.selectedFocalLength
-vm.selectedFocalDisctance
-vm.selectedAperture
-*/
+
+        if (fieldsAreValid()){
+            snapSave();
+        } else {
+            toastr.warning('Not all require fields met.');
+        }
+    }
+
+    function reset(){
+        toastr.info('New series initialized, please recheck the values.');
+        setInitValues();
     }
 
     function availableCameras(){
@@ -65,20 +70,55 @@ vm.selectedAperture
         return logService.getApertures();
     }
 
-    function success() {
-      toastr.success('success')
+    // internal functions
+    function fieldsAreValid(){
+        // if fails, return false;
+        return true;
     }
 
-    function warning() {
-      toastr.warning('warning')
+    function snapSave(){
+
+        var fileName,
+            prefix = vm.selectedFilePattern.split('#')[0],
+            padding = vm.selectedFilePattern.replace(prefix,'').length;
+
+        fileName = prefix + ('0000000000' + vm.selectedFileNumber).slice(-padding);
+
+        var url = '../server/snapSave.php',
+            data = {
+                "cameraName" : vm.selectedCamera,
+                "lensName" : vm.selectedLens,
+                "fileName" : fileName,
+                "seriesName" : vm.selectedSeriesName,
+                "focalLength" : vm.selectedFocalLength,
+                "focalDistance" : vm.selectedFocalDistance,
+                "apertureSize" : vm.selectedAperture,
+                "fileDate" : null,
+                "location" : vm.selectedLocation
+            };
+
+        $http({
+            method: 'POST',
+            url: url,
+            data: data
+            }).then(function successCallback(response) {
+
+                toastr.success('File '+ fileName +' saved.');
+                snapUpdate();
+
+            }, function errorCallback(response) {
+                toastr.error('File '+ fileName +' not saved. Use offline storage?');
+            });
+
+
     }
 
-    function info() {
-      toastr.info('info')
-    }
+    function snapUpdate(){
 
-    function error() {
-      toastr.error('error')
+        vm.selectedFileNumber = parseInt(vm.selectedFileNumber + 1);
+        vm.NumberOfSaved += 1;
+
+        console.log('Updating...')
     }
 
   }
